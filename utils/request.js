6f2e9b06e1afa05e20235cf0ed1eaa4dea2dc083
Sylvain Faucherand <syl.faucherand@gmail.com>
2015-11-09 15:13:30 +0000

    Reorg file tree

--- utils/request.js
+++ utils/request.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const { ACCESS_TOKEN } = require('../config');
-let agent = require('superagent-promise')(require('superagent'), Promise);
+const agent = require('superagent-promise')(require('superagent'), Promise);
 
-exports.request = (url) => {
+module.exports = (url) => {
   console.log('requesting', url);
   return agent.
     get(url)
