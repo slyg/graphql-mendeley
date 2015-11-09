a55409e8708f227ea57af274a9f934ffc6ed08af
Sylvain Faucherand <syl.faucherand@gmail.com>
2015-11-09 14:59:05 +0000

    Add query on profile

--- test/raw.sh
+++ test/raw.sh
@@ -1,6 +1,14 @@
-
+echo '\n\nGet profile\n'
 curl -XPOST -H "Content-Type:application/graphql"  -d '{
-    group(id:"e3630413-abd9-3308-8937-c5f119c17a28") {
+    profile(id: "1c3ac854-1c3c-3202-9753-93b69dd1566f") {
+      name,
+      title
+    }
+}' http://localhost:3000/graphql
+
+echo '\n\nGet members of a group\n'
+curl -XPOST -H "Content-Type:application/graphql"  -d '{
+    group(id: "e3630413-abd9-3308-8937-c5f119c17a28") {
       id,
       name,
       owner {
@@ -11,8 +19,7 @@ curl -XPOST -H "Content-Type:application/graphql"  -d '{
         id,
         role,
         profile {
-          id,
-          name
+          name,
         },
       },
     }
