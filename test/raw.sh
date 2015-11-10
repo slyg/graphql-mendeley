0415c97f3f2feb292f5c7b37fc1921e7e2689886
Sylvain Faucherand <syl.faucherand@gmail.com>
2015-11-10 16:38:54 +0000

    Add limit on academic statuses list

--- test/raw.sh
+++ test/raw.sh
@@ -1,8 +1,13 @@
-echo '\n\nGet profile\n'
+
 curl -XPOST -H "Content-Type:application/graphql" -d '{
-    profile(id: "84dfd8f8-cd91-33d9-8eda-c407f2b72f73") {
-      name,
-      title,
+    academicStatuses(limit: 2) {
+      description,
+      profiles(limit: 1) {
+        name,
+        documents {
+          title
+        }
+      }
     }
 }' http://localhost:3000/graphql
 sleep 1
@@ -10,17 +15,17 @@ sleep 1
 echo '\n\nGet members of a group\n'
 curl -XPOST -H "Content-Type:application/graphql" -d '{
     group(id: "e3630413-abd9-3308-8937-c5f119c17a28") {
-      id,
       name,
       owner {
         id,
         name,
       },
-      members(limit: 3) {
+      members(limit: 10) {
         id,
         role,
         profile {
           name,
+          title
         },
       },
     }
